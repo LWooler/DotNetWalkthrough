2f89fd6501f6b7f2344a3df707e199e8e015fa12
LWooler <lwooler@att.net>
2021-05-10 10:09:53 -0500

    dotnet installed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetWalkthrough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
